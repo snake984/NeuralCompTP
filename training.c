6b9df921905b1859a48523ce7d6765d2d98e6c8d
Tarek <smirani@et.esiea.fr>
2014-02-26 15:13:56 +0100

    merge

--- training.c
+++ training.c
@@ -7,10 +7,10 @@ int main()
     const unsigned int num_input = 26;
     const unsigned int num_output = 1;
     const unsigned int num_layers = 3;
-    const unsigned int num_neurons_hidden = 13;
+    const unsigned int num_neurons_hidden = 15;
     const float desired_error = (const float) 0.001;
     const unsigned int max_epochs = 500000;
-    const unsigned int epochs_between_reports = 5000;
+    const unsigned int epochs_between_reports = 100000;
 
 
     struct fann *ann = fann_create_standard(num_layers, num_input,
@@ -19,7 +19,7 @@ int main()
     fann_set_activation_function_hidden(ann, FANN_SIGMOID_SYMMETRIC);
     fann_set_activation_function_output(ann, FANN_SIGMOID_SYMMETRIC);
 
-    fann_train_on_file(ann, "sample.txt", max_epochs,
+    fann_train_on_file(ann, "training.txt", max_epochs,
         epochs_between_reports, desired_error);
 
     fann_save(ann, "network.net");
